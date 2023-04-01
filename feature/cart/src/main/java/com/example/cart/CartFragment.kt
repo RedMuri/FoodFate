d207736f602779fde9b5f5e076d5adb29a2afa6b
RedMuri <celovekda@gmail.com>
2023-04-01 16:38:31 +0300

    Add viewBinding in cart module

--- feature/cart/src/main/java/com/example/cart/CartFragment.kt
+++ feature/cart/src/main/java/com/example/cart/CartFragment.kt
@@ -1,19 +1,25 @@
 package com.example.cart
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cart.databinding.FragmentCartBinding
 
 class CartFragment : Fragment() {
 
 
+    private var _binding: FragmentCartBinding? = null
+    private val binding: FragmentCartBinding
+        get() = _binding ?: throw RuntimeException("FragmentCartBinding == null")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+    ): View {
+        _binding = FragmentCartBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
 }
--- feature/cart/src/main/java/com/example/cart/SuccessFragment.kt
+++ feature/cart/src/main/java/com/example/cart/SuccessFragment.kt
@@ -5,15 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.cart.databinding.FragmentSuccessBinding
 
 class SuccessFragment : Fragment() {
 
+    private var _binding: FragmentSuccessBinding? = null
+    private val binding: FragmentSuccessBinding
+        get() = _binding ?: throw RuntimeException("FragmentSuccessBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_success, container, false)
+    ): View {
+        _binding = FragmentSuccessBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
