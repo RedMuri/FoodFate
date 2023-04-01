0c54a6693005dfc2a1994d48a2955f401591003f
RedMuri <celovekda@gmail.com>
2023-04-02 00:24:32 +0300

    Add sign in viewModel

--- feature/auth/src/main/java/com/example/auth/AuthState.kt
+++ feature/auth/src/main/java/com/example/auth/AuthState.kt
@@ -4,4 +4,4 @@ sealed class AuthState()
 
 object AuthProgress : AuthState()
 class AuthError(val errorCode: Int): AuthState()
-class AuthSuccess(val firstName: String): AuthState()
+object AuthSuccess: AuthState()
