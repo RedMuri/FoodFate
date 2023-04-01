cce7a354191711b5dbbdba1b65676e7e55bc09d4
RedMuri <celovekda@gmail.com>
2023-04-01 17:42:00 +0300

    Add AppDatabase class

---
+++ core/db/src/main/java/com/example/db/AppDatabase.kt
@@ -0,0 +1,33 @@
+package com.example.db
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(version = 7, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    companion object {
+
+        private var INSTANCE: AppDatabase? = null
+        private const val DB_NAME = "food_fate.db"
+        private val LOCK = Any()
+
+        fun getInstance(application: Application): AppDatabase {
+            INSTANCE?.let { return it }
+            synchronized(LOCK) {
+                INSTANCE?.let { return it }
+                val db =
+                    Room.databaseBuilder(application, AppDatabase::class.java, DB_NAME)
+                        .fallbackToDestructiveMigration()
+                        .build()
+                INSTANCE = db
+                return db
+            }
+        }
+
+        const val USERS_TABLE_NAME = "users"
+    }
+
+}
