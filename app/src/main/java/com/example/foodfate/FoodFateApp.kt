3891cb69d9edc8baecfe2405d150c80f518578e3
RedMuri <celovekda@gmail.com>
2023-04-02 00:37:59 +0300

    Add main application class for injecting

---
+++ app/src/main/java/com/example/foodfate/FoodFateApp.kt
@@ -0,0 +1,24 @@
+package com.example.foodfate
+
+import android.app.Application
+import com.example.auth.di.AuthComponent
+import com.example.auth.di.AuthComponentProvider
+import com.example.foodfate.di.DaggerAppComponent
+
+class FoodFateApp : Application(), AuthComponentProvider {
+
+    val component by lazy {
+        DaggerAppComponent
+            .factory()
+            .create(this)
+    }
+
+    override fun onCreate() {
+        component.inject(this)
+        super.onCreate()
+    }
+
+    override fun getAuthComponent(): AuthComponent {
+        return component
+    }
+}
