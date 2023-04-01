df7d2752420dbbc027c9a283c56ef8c4417ee9d5
RedMuri <celovekda@gmail.com>
2023-04-02 02:15:22 +0300

    Add navigating from mainPage to meals on eatery click

--- feature/mainpage/src/main/java/com/example/mainpage/MainPageFragment.kt
+++ feature/mainpage/src/main/java/com/example/mainpage/MainPageFragment.kt
@@ -20,27 +20,38 @@ class MainPageFragment : Fragment() {
         EateriesAdapter()
     }
 
+    private val navigator by lazy {
+        (requireActivity() as MainPageNavigation)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        _binding = FragmentMainPageBinding.inflate(inflater,container,false)
+        _binding = FragmentMainPageBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupRecyclerView()
+        bindClickListeners()
+    }
+
+    private fun bindClickListeners() {
+        adapterEateries.onItemClickListener = {
+            navigator.navigateFromMainPageToMealsPage()
+        }
     }
 
     private fun setupRecyclerView() {
         binding.rvEateries.adapter = adapterEateries
         val testEateries = listOf(
-            Eatery(1,"Ресторан «Один»","С 9:00 до 18:00","4,8"),
-            Eatery(2,"Ресторан «Два»","С 9:00 до 18:00","4,7"),
-            Eatery(3,"Ресторан «Три»","С 9:00 до 18:00","4,7"),
-            Eatery(4,"Ресторан «Четыре»","С 9:00 до 18:00","4,6"),
-            Eatery(5,"Ресторан «Пять»","С 9:00 до 18:00","4,5"),
+            Eatery(1, "Ресторан «Один»", "С 9:00 до 18:00", "4,8"),
+            Eatery(2, "Ресторан «Два»", "С 9:00 до 18:00", "4,7"),
+            Eatery(3, "Ресторан «Три»", "С 9:00 до 18:00", "4,7"),
+            Eatery(4, "Ресторан «Четыре»", "С 9:00 до 18:00", "4,6"),
+            Eatery(5, "Ресторан «Пять»", "С 9:00 до 18:00", "4,5"),
         )
         adapterEateries.submitList(testEateries)
     }
