55fda731c2d849e19e3e84b5da3f03811f3e7589
RedMuri <celovekda@gmail.com>
2023-04-01 16:23:50 +0300

    Add viewBinding in auth fragments

--- feature/auth/src/main/java/com/example/auth/SignInFragment.kt
+++ feature/auth/src/main/java/com/example/auth/SignInFragment.kt
@@ -5,13 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.auth.databinding.FragmentSignInBinding
 
 class SignInFragment: Fragment() {
+
+    private var _binding: FragmentSignInBinding? = null
+    private val binding: FragmentSignInBinding
+        get() = _binding ?: throw RuntimeException("FragmentSignInBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    ): View {
+        _binding = FragmentSignInBinding.inflate(inflater,container,false)
+        return binding.root
     }
 }
