ec872dd835aa76203d036d100eb4a3b4a139a6b3
RedMuri <celovekda@gmail.com>
2023-04-02 13:00:47 +0300

    Add adapter for meals in cart

---
+++ feature/cart/src/main/java/com/example/cart/adapters/MealsAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.cart.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.cart.databinding.RvCartItemMealBinding
+import com.example.domain.model.Meal
+import com.example.meals.adapters.MealDiffCallback
+
+class MealsAdapter() :
+    ListAdapter<Meal, MealsAdapter.MealViewHolder>(
+        MealDiffCallback()
+    ) {
+
+    class MealViewHolder(val binding: RvCartItemMealBinding) :
+        ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MealViewHolder {
+        val binding = RvCartItemMealBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return MealViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MealViewHolder, position: Int) {
+        with(holder.binding) {
+            with(currentList[position]) {
+                tvMealName.text = name
+                tvMealPrice.text = String.format("%s₽", price.toString())
+                tvMealWeight.text = String.format("%s г.", weight.toString())
+                tvMealDiscount.text = String.format("-%s%%", discount.toString())
+            }
+        }
+    }
+}
