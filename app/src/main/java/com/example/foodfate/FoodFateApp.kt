f5c6a94b3da7aaaf2e1a752dce4d185b2c2f1bfc
RedMuri <celovekda@gmail.com>
2023-04-20 02:39:02 +0300

    Add map

--- app/src/main/java/com/example/foodfate/FoodFateApp.kt
+++ app/src/main/java/com/example/foodfate/FoodFateApp.kt
@@ -3,10 +3,11 @@ package com.example.foodfate
 import android.app.Application
 import com.example.auth.di.AuthComponent
 import com.example.auth.di.AuthComponentProvider
+import com.yandex.mapkit.MapKitFactory
 
 class FoodFateApp : Application(), AuthComponentProvider {
 
-    val component by lazy {
+    private val component by lazy {
         DaggerAppComponent
             .factory()
             .create(this)
@@ -14,10 +15,16 @@ class FoodFateApp : Application(), AuthComponentProvider {
 
     override fun onCreate() {
         component.inject(this)
+        MapKitFactory.setApiKey(MAPKIT_API_KEY)
         super.onCreate()
     }
 
     override fun getAuthComponent(): AuthComponent {
         return component
+    }
+
+    companion object {
+
+        private const val MAPKIT_API_KEY = "5f44d376-56e6-4bcd-b4e7-d7c9ee03c698"
     }
 }
--- feature/map/src/main/java/com/example/map/MapFragment.kt
+++ feature/map/src/main/java/com/example/map/MapFragment.kt
@@ -1,18 +1,65 @@
 package com.example.map
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.map.databinding.FragmentMapBinding
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+
 
 class MapFragment : Fragment() {
+
+    private var _binding: FragmentMapBinding? = null
+    private val binding: FragmentMapBinding
+        get() = _binding ?: throw RuntimeException("FragmentMapBinding == null")
+
+    override fun onAttach(context: Context) {
+        MapKitFactory.initialize(requireContext())
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+    ): View {
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupMap()
+    }
+
+
+    private fun setupMap() {
+        binding.mapview.map.move(
+            CameraPosition(Point(DEFAULT_LATITUDE, DEFAULT_LONGITUDE), 12.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 0F),
+            null
+        )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        MapKitFactory.getInstance().onStop()
+        binding.mapview.onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        binding.mapview.onStart()
+    }
+
+    companion object {
+        private const val DEFAULT_LATITUDE = 57.6299
+        private const val DEFAULT_LONGITUDE = 39.8737
     }
 }
